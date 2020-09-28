9870c59712c0631c195b0252ca427f049d8decc9
Linnea <linneagear@gmail.com>
2020-09-28 14:28:44 -0700

    Changed title and edited readme

---
+++ build/precache-manifest.faeda4c2b26c6db4378fe78d3792b317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0384d2315314418cfc210446dd250fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "432689089ac834cfb9e1",
+    "url": "/static/css/main.4aa0c089.chunk.css"
+  },
+  {
+    "revision": "326f94011ecf998b2f49",
+    "url": "/static/js/2.7f09754e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f09754e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432689089ac834cfb9e1",
+    "url": "/static/js/main.c1f8e7e1.chunk.js"
+  },
+  {
+    "revision": "627d948798b48f45f252",
+    "url": "/static/js/runtime-main.d5bc699f.js"
+  }
+]);
